747cd9fdb5362aca65866df6d3ac5cb3b260f082
saurabhkoshatwar <35650601+saurabhkoshatwar@users.noreply.github.com>
2018-08-31 12:10:09 +0530

    cmd done

--- client/FileClient.java
+++ client/FileClient.java
@@ -5,15 +5,17 @@ public class FileClient {
 
   public static void main(String args[])
   {
-                /*
+                
                 Scanner sc = new Scanner(System.in);
-                //System.out.println();
+                
                 System.out.print(">>>>");
                 String inp = sc.next();
                 String location = "local";
                 
                 while(!inp.equals("exit"))
                 {
+			
+			
                         if(inp.equals("ls"))
                         {
                                 if(location.equals("local"))
@@ -27,13 +29,13 @@ public class FileClient {
 		                        FileServerIntf fileServerIntf = (FileServerIntf)Naming.lookup(fileServerURL);
 		                        
 		                         //System.out.println("The first number is: " + args[1]);
-		                         		double d1 = Double.valueOf(args[1]).doubleValue();
+		                         		//double d1 = Double.valueOf(args[1]).doubleValue();
 		                         		//System.out.println("The second number is: " + args[2]);
 		                        //double d2 = Double.valueOf(args[2]).doubleValue(); 
 		                        //System.out.println("The sum is: " + addServerIntf.add(d1, d2));
 		                        
 
-		                        System.out.println(fileServerIntf.listfiles());
+		                        System.out.println(fileServerIntf.listfiles(inp));
 
 		                        } 
 
@@ -44,15 +46,24 @@ public class FileClient {
                  	}
                  	else if(inp.contains("cd"))
                  	{
-                 	        
-                 	
+				
+                 	        if(inp.contains("Movies"))
+				{
+					
+					location = "10.11.3.17";
+				}
+				else if(inp.contains("Videos"))
+				{
+					
+					location = "10.11.3.18";
+				}
                  	}
                  	
                  	System.out.print(">>>>");
-                 	inp = sc.next();	
+                 	inp = sc.nextLine();	
          	}
-         	*/
          	
+         	/*
                 Scanner sc = new Scanner(System.in);
                 System.out.print(">>>>");
                 String inp = sc.next();  
@@ -73,7 +84,7 @@ public class FileClient {
                 
                  	System.out.print(">>>>");
                  	inp = sc.next();	
-         	}   
+         	}*/   
    
          		
    }
